63249a4a8161f28dd67b21bc45073d29052f25d3
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2026-03-22 21:50:40 -0500

    feat(sample-additional-fields): add option payload dto

---
+++ src/main/java/org/openelisglobal/sample/bean/SampleTypeAdditionalFieldOptionPayload.java
@@ -0,0 +1,50 @@
+package org.openelisglobal.sample.bean;
+
+public class SampleTypeAdditionalFieldOptionPayload {
+
+    private Integer id;
+    private String optionKey;
+    private String optionLabel;
+    private Integer sortOrder;
+    private Boolean active;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOptionKey() {
+        return optionKey;
+    }
+
+    public void setOptionKey(String optionKey) {
+        this.optionKey = optionKey;
+    }
+
+    public String getOptionLabel() {
+        return optionLabel;
+    }
+
+    public void setOptionLabel(String optionLabel) {
+        this.optionLabel = optionLabel;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
