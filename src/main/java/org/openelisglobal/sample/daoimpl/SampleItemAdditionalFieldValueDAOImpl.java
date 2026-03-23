538b7dc4bd0d888307f80dab0bd2e8652cf18f93
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2026-03-22 21:50:40 -0500

    feat(sample-additional-fields): implement sample item additional value dao

---
+++ src/main/java/org/openelisglobal/sample/daoimpl/SampleItemAdditionalFieldValueDAOImpl.java
@@ -0,0 +1,45 @@
+package org.openelisglobal.sample.daoimpl;
+
+import java.util.List;
+import java.util.Optional;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.openelisglobal.common.daoimpl.BaseDAOImpl;
+import org.openelisglobal.sample.dao.SampleItemAdditionalFieldValueDAO;
+import org.openelisglobal.sample.valueholder.SampleItemAdditionalFieldValue;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class SampleItemAdditionalFieldValueDAOImpl extends BaseDAOImpl<SampleItemAdditionalFieldValue, Integer>
+        implements SampleItemAdditionalFieldValueDAO {
+
+    public SampleItemAdditionalFieldValueDAOImpl() {
+        super(SampleItemAdditionalFieldValue.class);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<SampleItemAdditionalFieldValue> findBySampleItemId(Integer sampleItemId) {
+        String hql = "FROM SampleItemAdditionalFieldValue v WHERE v.sampleItemId = :sampleItemId ORDER BY v.id ASC";
+        Query<SampleItemAdditionalFieldValue> query = entityManager.unwrap(Session.class).createQuery(hql,
+                SampleItemAdditionalFieldValue.class);
+        query.setParameter("sampleItemId", sampleItemId);
+        return query.list();
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<SampleItemAdditionalFieldValue> findBySampleItemIdAndFieldDefinitionId(Integer sampleItemId,
+            Integer fieldDefinitionId) {
+        String hql = "FROM SampleItemAdditionalFieldValue v WHERE v.sampleItemId = :sampleItemId "
+                + "AND v.fieldDefinitionId = :fieldDefinitionId";
+        Query<SampleItemAdditionalFieldValue> query = entityManager.unwrap(Session.class).createQuery(hql,
+                SampleItemAdditionalFieldValue.class);
+        query.setParameter("sampleItemId", sampleItemId);
+        query.setParameter("fieldDefinitionId", fieldDefinitionId);
+        SampleItemAdditionalFieldValue value = query.uniqueResult();
+        return Optional.ofNullable(value);
+    }
+}
