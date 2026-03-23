faa63f9f37115c9b64fb5ee534c7fab2131aef3a
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2026-03-22 21:50:40 -0500

    feat(sample-additional-fields): add sample item additional value dao

---
+++ src/main/java/org/openelisglobal/sample/dao/SampleItemAdditionalFieldValueDAO.java
@@ -0,0 +1,14 @@
+package org.openelisglobal.sample.dao;
+
+import java.util.List;
+import java.util.Optional;
+import org.openelisglobal.common.dao.BaseDAO;
+import org.openelisglobal.sample.valueholder.SampleItemAdditionalFieldValue;
+
+public interface SampleItemAdditionalFieldValueDAO extends BaseDAO<SampleItemAdditionalFieldValue, Integer> {
+
+    List<SampleItemAdditionalFieldValue> findBySampleItemId(Integer sampleItemId);
+
+    Optional<SampleItemAdditionalFieldValue> findBySampleItemIdAndFieldDefinitionId(Integer sampleItemId,
+            Integer fieldDefinitionId);
+}
