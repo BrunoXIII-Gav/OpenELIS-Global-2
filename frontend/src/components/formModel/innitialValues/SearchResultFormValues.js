4f4b16bbb9862479c3f06b4655437bd50c10f693
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2026-04-15 12:07:21 -0500

    feat(results-ui): add initial state for additional result fields

--- frontend/src/components/formModel/innitialValues/SearchResultFormValues.js
+++ frontend/src/components/formModel/innitialValues/SearchResultFormValues.js
@@ -104,6 +104,9 @@ export default {
       upperNormalRange: "",
       userChoiceReflex: "",
       valid: "",
+      additionalFieldDefinitions: [],
+      additionalFieldValues: {},
+      additionalFieldShadowValues: {},
     },
   ],
 
