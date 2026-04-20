637d283ed7e0b07412b3ef772bea6546633516f1
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2026-04-19 20:14:04 -0500

    feat(order-additional-fields): add fixed field config payload dto

---
+++ src/main/java/org/openelisglobal/orderadditionalfield/bean/OrderFixedFieldConfigPayload.java
@@ -0,0 +1,59 @@
+package org.openelisglobal.orderadditionalfield.bean;
+
+public class OrderFixedFieldConfigPayload {
+
+    private Integer id;
+    private String fieldKey;
+    private Boolean visible;
+    private Boolean required;
+    private Boolean readonly;
+    private Integer sortOrder;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFieldKey() {
+        return fieldKey;
+    }
+
+    public void setFieldKey(String fieldKey) {
+        this.fieldKey = fieldKey;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public Boolean getRequired() {
+        return required;
+    }
+
+    public void setRequired(Boolean required) {
+        this.required = required;
+    }
+
+    public Boolean getReadonly() {
+        return readonly;
+    }
+
+    public void setReadonly(Boolean readonly) {
+        this.readonly = readonly;
+    }
+
+    public Integer getSortOrder() {
+        return sortOrder;
+    }
+
+    public void setSortOrder(Integer sortOrder) {
+        this.sortOrder = sortOrder;
+    }
+}
