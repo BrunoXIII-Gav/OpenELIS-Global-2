77ccb604bf7d982512d0c2d8451b2a1cbb4fce5c
BrunoXIII-Gav <bruno.gavidia@upch.pe>
2026-04-27 04:38:13 -0500

    fix(notification): cap stat message length

--- src/main/java/org/openelisglobal/result/controller/rest/LogbookResultsRestController.java
+++ src/main/java/org/openelisglobal/result/controller/rest/LogbookResultsRestController.java
@@ -551,7 +551,6 @@ public class LogbookResultsRestController extends LogbookResultsBaseController {
                     .collect(Collectors.toList());
             List<String> systemUserIds = userRoleService.getUserIdsForRole(Constants.ROLE_VALIDATION);
             String message = MessageUtil.getMessage("notification.result.stat");
-            StringBuffer sb = new StringBuffer(message);
             for (String userId : systemUserIds) {
                 List<Analysis> userAnalyses = userService
                         .filterAnalysesByLabUnitRoles(userId, newResultAnalyses, Constants.ROLE_VALIDATION).stream()
@@ -565,15 +564,19 @@ public class LogbookResultsRestController extends LogbookResultsBaseController {
                                     + " - " + a.getTest().getLocalizedName())
                             .collect(Collectors.toList());
                     String testString = String.join(", ", userTests);
-                    sb.append(testString);
                     try {
                         Notification notification = new Notification();
-                        notification.setMessage(sb.toString());
+                        String notificationMessage = message + testString;
+                        if (notificationMessage.length() > 255) {
+                            notificationMessage = notificationMessage.substring(0, 255);
+                        }
+                        notification.setMessage(notificationMessage);
                         notification.setUser(systemUserService.getUserById(userId));
                         notification.setCreatedDate(OffsetDateTime.now());
                         notification.setReadAt(null);
                         notificationDAO.save(notification);
                     } catch (Exception e) {
+                        LogEvent.logError(e);
                     }
                 }
             }
